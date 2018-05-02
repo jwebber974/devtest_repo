ec61aea6d87f7cb1bc37691cb1c574385c5d96a6
MEPSREAL-TIME\jwebber <jwebber@mepsrealtime.com>
2018-05-02 11:24:45 -0700

    Initial Commit

---
+++ IntelliGuardTest/Common/DatabaseAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace IntelliGuardTest.Common
+{
+    public class DatabaseAccess
+    {
+        public IEnumerable<T> Query<T>(string storedProcedureName, IEnumerable<SqlParameter> sqlParameters = null) where T : new()
+        {
+            return Enumerable.Range(0, storedProcedureName.Length).Select(x => GenerateRandomClass<T>());
+        }
+
+        public T ExecuteScalar<T>(string storedProcedureName, IEnumerable<SqlParameter> sqlParameters = null) where T : new()
+        {
+            return GenerateRandomClass<T>();
+        }
+
+        private T GenerateRandomClass<T>() where T : new()
+        {
+            Random rnd = new Random();
+            var index = 0;
+            var result = new T();
+            var type = typeof(T);
+            var propertyInfo = type.GetProperties();
+            foreach (PropertyInfo pInfo in propertyInfo)
+            {
+                if (!pInfo.CanWrite) { continue; }
+                if (pInfo.PropertyType == typeof(int))
+                {
+                    pInfo.SetValue(result, rnd.Next(1, 200));
+                    index++;
+                }
+                if (pInfo.PropertyType == typeof(string))
+                {
+                    pInfo.SetValue(result, Path.GetRandomFileName().Replace(".", ""));
+                    index++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ IntelliGuardTest/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using IntelliGuardTest.Common;
+using IntelliGuardTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace IntelliGuardTest.Services
+{
+    public class UserService
+    {
+        private readonly DatabaseAccess _databaseAccess;
+        public UserService()
+        {
+            _databaseAccess = new DatabaseAccess();
+        }
+
+        public IEnumerable<User> SelectAll()
+        {
+            return _databaseAccess.Query<User>("users.SelectAll");
+        }
+
+        /// <summary>
+        /// Inserts the new user and returns the user id
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public int Add(User user)
+        {
+            return _databaseAccess.ExecuteScalar<int>("users.AddUser", new SqlParameter[] {
+                new SqlParameter("@FirstName",user.FirstName),
+                new SqlParameter("@LastName",user.LastName),
+                new SqlParameter("@Email",user.Email)
+            });
+        }
+
+        public void Edit(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
