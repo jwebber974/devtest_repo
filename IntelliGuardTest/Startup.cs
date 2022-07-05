834a6d5222e73a721cf5d807c96047eb79b1654f
Jeff Webber <jwebber@ig.solutions>
2022-07-05 15:31:58 -0700

    Update references

--- IntelliGuardTest/Startup.cs
+++ IntelliGuardTest/Startup.cs
@@ -22,10 +22,11 @@ namespace IntelliGuardTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
